fa84ae52097638dcba899b3cbe4dccbd7def2bbc
KiraDitt <kiradittmer@gmail.com>
2025-05-01 10:49:09 +0200

    Added routing and added Home option to MenuBar

--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -1,34 +1,51 @@
 import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import SearchIcon from '@mui/icons-material/Search';
-import { useState } from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function MenuBar() {
-  const [value, setValue] = useState(0);
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  return (
-    <Paper
-      sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-      elevation={3}
-    >
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }} sx={{ '& .Mui-selected': {
-            color: '#3f554d',
-            fontWeight: 'bold',
-        } }}
-      >
-        <BottomNavigationAction label="My Books" icon={<MenuBookIcon sx= {{ color: '#3f554d'}}/>} />
-        <BottomNavigationAction label="Search" icon={<SearchIcon  sx= {{ color: '#3f554d'}}/>} />
-      </BottomNavigation>
-    </Paper>
-  );
+    return (
+        <Paper
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                right: 0,
+            }}
+            elevation={3}
+        >
+            <BottomNavigation
+                showLabels
+                value={location.pathname} // Matches current route
+                onChange={(event, newValue) => {
+                    navigate(newValue); // Navigate to new route
+                }} sx={{
+                    '& .Mui-selected': {
+                        color: '#3f554d',
+                        fontWeight: 'bold',
+                    }
+                }}
+            >
+                <BottomNavigationAction
+                    label="Home"
+                    value="/"
+                    icon={<HomeIcon sx={{ color: '#3f554d' }} />}
+                />
+                <BottomNavigationAction
+                    label="My Books" 
+                    value="/my-books"
+                    icon={<MenuBookIcon sx={{ color: '#3f554d' }} />} 
+                />
+                <BottomNavigationAction 
+                    label="Search"
+                    value="/search" 
+                    icon={<SearchIcon sx={{ color: '#3f554d' }} />} 
+                />
+            </BottomNavigation>
+        </Paper>
+    );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import MyBooks from './pages/MyBooks';
+import Search from './pages/Search';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/my-books" element={<MyBooks />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
+  </BrowserRouter>
+);
+
